f5e385f557af637ccd325d2d576a02ff57af87b2
m5tibi <vargat77@gmail.com>
2025-11-07 17:23:14 +0100

    Delete run.sh

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-python3 -m venv .venv
-source .venv/bin/activate
-pip install -r requirements.txt
-if [ ! -f .env ]; then cp .env.template .env; fi
-python livemesterbot.py
